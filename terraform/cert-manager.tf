acec705e88510c0e99f7c53749fc658a0349d835
Santi Albi26 <santi.albisetti>
2026-05-26 16:15:52 -0300

    feat: add TLS with cert-manager self-signed certificate

---
+++ terraform/cert-manager.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "v1.14.5"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [aws_eks_node_group.main]
+}
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -98,8 +98,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 3
+    max_size     = 4
     min_size     = 1
   }
 
